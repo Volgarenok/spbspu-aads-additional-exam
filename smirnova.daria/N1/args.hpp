78d8a1b6888345a8d77138e8ca2503ad07e45a7c
dassrm <daryasm1rnoff@yandex.ru>
2026-09-22 09:00:38 +0300

    Creating the main function and the command‑line parameter parser.

---
+++ smirnova.daria/N1/args.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+namespace smirnova {
+  struct Args {
+    std::string in_file;
+    std::string out_file;
+    bool has_in;
+    bool has_out;
+  };
+
+  int parse_args(const int argc, char* argv[], Args& args);
+}
---
+++ smirnova.daria/N1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "args.hpp"
+
+int main(int argc, char* argv[]) {
+  smirnova::Args args;
+  int parse_result = smirnova::parse_args(argc, argv, args);
+  if (parse_result != 0) {
+    return parse_result;
+  }
+  return 0;
+}
+
