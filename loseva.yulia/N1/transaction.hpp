2ffd28bcc523456eac9ca155b2b61168bc201d1e
yuklya <yuloseva@mail.ru>
2026-09-24 20:25:22 +0300

    fix cg in transactin

--- loseva.yulia/N1/transaction.hpp
+++ loseva.yulia/N1/transaction.hpp
@@ -1,20 +1,22 @@
-#ifndef LOSEVA_TRANSACTION_HPP
-#define LOSEVA_TRANSACTION_HPP
+#ifndef TRANSACTION_HPP
+#define TRANSACTION_HPP
 
-#include <iostream>
+#include <cstddef>
+#include <istream>
+#include <ostream>
 #include "dynamic_array.hpp"
 
 namespace loseva {
 
 struct Transaction {
-  int from_;
-  int to_;
-  int value_;
+  int from;
+  int to;
+  int value;
 };
 
 struct ReadStats {
-  std::size_t success_count_;
-  std::size_t ignored_count_;
+  std::size_t success_count;
+  std::size_t ignored_count;
 };
 
 void readTransactions(std::istream &in, DynamicArray<Transaction> &arr, ReadStats &stats);
@@ -23,3 +25,4 @@ void printTransactions(std::ostream &out, const DynamicArray<Transaction> &arr);
 }
 
 #endif
+
