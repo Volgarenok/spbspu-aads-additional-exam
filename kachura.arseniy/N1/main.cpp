f79fba58899f3f7e4c62cd2add0a3a53fa8f805f
pelmon <kacuraarsenij@gmail.com>
2026-09-24 19:55:10 +0300

    changed input processing

--- kachura.arseniy/N1/main.cpp
+++ kachura.arseniy/N1/main.cpp
@@ -2,25 +2,28 @@
 #include <fstream>
 #include <string>
 
+template<typename T>
+void readLine(T* line, int* from_ptr, int* to_ptr, int* value_ptr)
+{
+  for(size_t i = 0; i < 3; i++)
+  {
+    *line >> *from_ptr;
+    *line >> *to_ptr;
+    *line >> *value_ptr;
+  }
+}
+
 int main(int argc, char* argv [])
 {
   int from, to, value;
+  to = 0;
+  value = 0;
   size_t succesful_records, ignored_records;
   std::string data;
   std::ifstream in ("input.txt");
   std::ofstream out ("output.txt");
-  for(size_t i = 0; in >> data; i++)
-  {
-    out << data;
-    if (i == 2)
-    {
-      out << '\n';
-    }
-    else
-    {
-      out << ' ';
-    }
-  }
+  readLine(&in, &from, &to, &value);
+  out << from << ' ' << to << ' ' << value << '\n';
   in.close();
   out.close();
 }
